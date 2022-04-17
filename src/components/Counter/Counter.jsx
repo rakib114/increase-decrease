53e487453e40d03149c3236541231cd6ef7eda09
rakib114 <mahmud17209@gmail.com>
2022-04-18 01:18:59 +0600

    add more button

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import "./Counter.css";
 const Counter = () => {
   const [counter, setCounter] = useState(0);
-  function increase() {
-    setCounter(counter + 1);
+  function increase(num) {
+    setCounter(counter + num);
   }
-  function decrease() {
-    setCounter(counter - 1);
-    if (counter === 0) {
+  function decrease(num) {
+    setCounter(counter - num);
+    if (counter === 0 || counter < 0) {
       setCounter(0);
       alert("you can't decrease no more");
     }
@@ -17,11 +17,17 @@ const Counter = () => {
       <div className="box_area">
         <h2 className="second_heading">Value of The counter is {counter} </h2>
         <div className="button_area">
-          <button className="btn" onClick={increase}>
+          <button className="btn" onClick={() => increase(1)}>
             Increase by One{" "}
           </button>
-          <button className="btn" onClick={decrease}>
+          <button className="btn" onClick={() => decrease(1)}>
             Decrease by One{" "}
+          </button>
+          <button className="btn" onClick={() => increase(5)}>
+            Increase by Five{" "}
+          </button>
+          <button className="btn" onClick={() => decrease(5)}>
+            Decrease by Five{" "}
           </button>
         </div>
       </div>
